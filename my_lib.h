3d43deb450c9690ec3d199f75d8a6bedeb1210a3
maks228998 <78788414+maks228998@users.noreply.github.com>
2021-03-29 16:07:30 +0600

    Update my_lib.h

--- my_lib.h
+++ my_lib.h
@@ -3,17 +3,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-//version 0.6
+//version 0.7
 
 int input_int_number();
-int get_int_rand_min_max(const int min, const int max);
 int get_int_number_on_positive();
 int get_int_number_on_one_or_zero();
 int get_int_number_than_greater_one();
 int get_int_number_than_greater_min_and_less_max(const int min, const int max);
+int get_int_rand_min_max(const int min, const int max);
+int get_int_factorial(const int number);
 
 double input_double_number();
 double double_rand_min_max(const double min, const double max);
 double get_double_number_than_greater_one();
 double get_double_number_on_positive();
 double get_double_number_than_greater_min_and_less_max(const double min, const double max);
+double get_double_factorial(const double number);
