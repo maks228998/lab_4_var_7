8ff5a84694674009dcd49acd43d637afa6a79087
maks228998 <78788414+maks228998@users.noreply.github.com>
2021-03-29 16:12:40 +0600

    Update my_lib.h

--- my_lib.h
+++ my_lib.h
@@ -9,7 +9,7 @@ int input_int_number();
 int get_int_number_on_positive();
 int get_int_number_on_one_or_zero();
 int get_int_number_than_greater_one();
-int get_int_number_than_greater_min_and_less_max(const int min, const int max);
+int get_int_number_than_not_greater_min_and_not_less_max(const int min, const int max);
 int get_int_rand_min_max(const int min, const int max);
 int get_int_factorial(const int number);
 
@@ -17,5 +17,5 @@ double input_double_number();
 double double_rand_min_max(const double min, const double max);
 double get_double_number_than_greater_one();
 double get_double_number_on_positive();
-double get_double_number_than_greater_min_and_less_max(const double min, const double max);
+double get_double_number_than_not_greater_min_and_not_less_max(const double min, const double max);
 double get_double_factorial(const double number);
